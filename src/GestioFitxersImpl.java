fb482db4736bc31c935449c4fac5e9690bcf82d8
Jordi Ribellas <ribellas2002@gmail.com>
2022-09-14 17:31:45 +0200

    M6 - First Commit

---
+++ src/GestioFitxersImpl.java
@@ -0,0 +1,206 @@
+import ioc.dam.m6.exemples.gestiofitxers.FormatVistes;
+import ioc.dam.m6.exemples.gestiofitxers.GestioFitxers;
+import ioc.dam.m6.exemples.gestiofitxers.GestioFitxersException;
+import ioc.dam.m6.exemples.gestiofitxers.TipusOrdre;
+
+public class GestioFitxersImpl implements GestioFitxers{
+
+	@Override
+	public void amunt() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void creaCarpeta(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void creaFitxer(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void elimina(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void entraA(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean esPotEscriure(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean esPotExecutar(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean esPotLlegir(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getAdrecaCarpeta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getColumnes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object[][] getContingut() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEspaiDisponibleCarpetaTreball() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEspaiTotalCarpetaTreball() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getFiles() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public FormatVistes getFormatContingut() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getInformacio(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getMostrarOcults() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getNomCarpeta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipusOrdre getOrdenat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getTitolColumnes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getUltimaModificacio(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String nomArrel(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int numArrels() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void reanomena(String arg0, String arg1) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setAdrecaCarpeta(String arg0) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setColumnes(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEsPotEscriure(String arg0, boolean arg1) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEsPotExecutar(String arg0, boolean arg1) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEsPotLlegir(String arg0, boolean arg1) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFormatContingut(FormatVistes arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMostrarOcults(boolean arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setOrdenat(TipusOrdre arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setUltimaModificacio(String arg0, long arg1) throws GestioFitxersException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
